0b6b04c91f2458ebbde5a032e3ea3da2c43ea150
befthimi <bill_efthimiou@hotmail.com>
2016-04-15 14:00:36 -0700

    class1 directory

---
+++ class1/class1_ex6.py
@@ -0,0 +1,31 @@
+# Class 1, Exercise 6
+# output is two files: myList.yml & myList.json
+#
+#!/usr/bin/env python
+ 
+import yaml
+import json
+import pprint
+
+myList = []
+
+# create a list beginning with 10 nums.
+for i in range (10):
+ myList.append(i)
+
+# Add a dict with 3 Australia states and with abbreviations
+myList.append({'NSW': 'New South Wales', 'VIC': 'Victoria', 'TAS': 'Tasmansia'})
+
+print "\nHere is my list:"
+print myList
+
+print "print in expanded YAML format to myList.yml\n"
+with open ("myList.yml" ,'w') as f:
+    f.write(yaml.dump(myList, default_flow_style=False))
+f.close()
+
+print "print in JSON format to myList.json\n"
+with open ("myList.json", 'w') as f:
+    json.dump(myList, f)
+f.close()
+
